e055dfe18b0828bd713a2b767df27347b56f205b
Ye Feng <ye@8i.com>
2018-01-11 17:09:13 +1300

    Fix init submit

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => [
-  {
-  	"name" : "Wellington",
-  	"ipaddr" : "12.34.56.78",
-  	"port" : "7788"
-  }])
+  .get('/', (req, res) => res.send([
+		{
+			"name": "Wellington",
+			"ipaddr": "10.20.10.182",
+			"port": "17788"
+		}
+  	]))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
